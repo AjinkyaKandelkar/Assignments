b7787f9ebc1a84153d80c919da3ead6fa011e616
Ajinkya Kandelkar <motoX554@gmail.com>
2022-05-23 18:42:30 +0530

    today

---
+++ Assignment 7/script.js
@@ -0,0 +1,33 @@
+// y=true;
+// function fun1()
+// {
+//    if(y==true)
+//     {  
+        
+//         document.getElementById("changecolor1").setAttribute("class","change_colorpurple");
+        
+//         y=false;
+//     }
+//      else
+//     {
+//         document.getElementById("changecolor1").setAttribute("class","change_colorblack");
+//         y=true;
+//     }
+
+// }
+// function fun2()
+// {
+//    if(y==true)
+//     {  
+        
+//         document.getElementById("changecolor2").setAttribute("class","change_colorpurple");
+        
+//         y=false;
+//     }
+//      else
+//     {
+//         document.getElementById("changecolor2").setAttribute("class","change_colorblack");
+//         y=true;
+//     }
+
+// }
