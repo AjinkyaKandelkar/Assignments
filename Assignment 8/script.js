6991517dca58f9c2b3408cef65d21e75b58b97a2
Ajinkya Kandelkar <motoX554@gmail.com>
2022-10-17 17:32:21 +0530

    changes

--- Assignment 8/script.js
+++ Assignment 8/script.js
@@ -25,10 +25,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     /*===== LINK ACTIVE =====*/
     const linkColor = document.querySelectorAll('.nav_link')
-    
     function colorLink(){
-    if(linkColor){
-    linkColor.forEach(l=> l.classList.remove('active'))
+        if(linkColor){
+            linkColor.forEach(l=> l.classList.remove('active')) 
     this.classList.add('active')
     }
     }
